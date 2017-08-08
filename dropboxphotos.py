ce92bb4d9bbeb39a6a502a08d9c736590be5d234
Laurent Jusseaume <laurent.jusseaume@yahoo.fr>
2017-08-08 21:22:54 +0200

    add upload function + a main app that asks usr which directory to upload

--- dropboxphotos.py
+++ dropboxphotos.py
@@ -6,10 +6,14 @@ def connect_to_dropbox(token):
     return dropbox.Dropbox(token)
 
 
-def print_missing_category(list_dropbox, list_local):
+def get_missing_category(list_dropbox, list_local):
     missing = []
-    for elem in list_dropbox:
-        if elem not in list_local:
+    lower_case_list_local = []
+    for elem in list_local:
+        lower_case_list_local.append(elem.lower())
+
+    for elem in lower_case_list_local:
+        if elem not in list_dropbox:
             missing.append(elem)
     return missing
 
@@ -20,9 +24,47 @@ def compare_dropbox_and_local(dbx, root_directory_dropbox, root_directory_local)
         list_dropbox = []
         list_local = listdir(root_directory_local + '\\' + year.name)
         for category in dbx.files_list_folder(root_directory_dropbox + '/' + year.name).entries:
-            list_dropbox.append(category.name)
-        missing_categories = print_missing_category(list_dropbox, list_local)
+            list_dropbox.append(category.name.lower())
+        missing_categories = get_missing_category(list_dropbox, list_local)
         if (missing_categories != []):
             missing[year.name] = missing_categories
     return missing
+
+
+def upload_new_folder(dbx, from_folder, to_folder, display_progress = True):
+    list_files = listdir(from_folder)
+
+    #remove thumbs.db if it exists
+    try:
+        index = list_files.index("Thumbs.db")
+        if index != -1:
+            del(list_files[index])
+    except ValueError as e:
+        pass
+
+    if display_progress == True:
+        print("copie de : " + str(len(list_files)) + " fichiers")
+
+    try:
+        dbx.files_create_folder(to_folder)
+    except Exception as e:
+        print(e)
+        return
+
+    number = 1
+    for file_name in list_files:
+        try:
+            file = open(from_folder + '\\' + file_name, "rb")
+            bytes_read = file.read(100*1024*1024)
+            file.close()
+            dbx.files_upload(bytes_read, to_folder + '/' + file_name)
+            if display_progress == True:
+                print(str(number))
+                number += 1
+        except Exception as e:
+            print(e)
+            if display_progress == True:
+                print("echec de transfert du fichier " + file_name)
+    if display_progress == True:
+        print("copie du repertoire finie")
 
---
+++ main_app_dropbox.py
@@ -0,0 +1,28 @@
+from dropboxphotos import *
+
+root_directory_dropbox = '/photos et videos'
+root_directory_local = 'f:\photos et videos'
+
+# read the dropbox token that is used to access the user's account
+file = open('token.txt', 'r')
+token = file.read()
+file.close()
+
+print("connexion a Dropbox")
+dbx = connect_to_dropbox(token)
+print("comparaison des arborescences")
+missing = compare_dropbox_and_local(dbx, root_directory_dropbox, root_directory_local)
+
+for year in sorted(missing.keys()):
+    number = 0
+    for elem in missing[year]:
+        print (year + ' numero ' + str(number) + ' : ' + elem)
+        number += 1
+
+year = input ('annee :')
+index = int (input('numero de repertoire : '))
+source_folder = root_directory_local + '\\'+ year + '\\' + missing[year][index]
+dest_folder = root_directory_dropbox + '/'+ year + '/' + missing[year][index]
+print(" copie de " + source_folder + " vers " + dest_folder)
+upload_new_folder(dbx, source_folder , dest_folder)
+
